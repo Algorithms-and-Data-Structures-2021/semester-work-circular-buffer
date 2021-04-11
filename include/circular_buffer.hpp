19475938d5b4d4b8258b68d879275b7b1ff0ae40
Orkhan Kazymov <rayskarken@gmail.com>
2021-04-11 16:13:04 +0300

    created structure

--- include/circular_buffer.hpp
+++ include/circular_buffer.hpp
@@ -1,27 +1,67 @@
 #pragma once
 
+#include <cstddef>
+#include <optional>
+
+using std::size_t;
 // Заголовочный файл с объявлением структуры данных
 
 namespace itis {
 
   // Tip 1: объявите здесь необходимые структуры, функции, константы и прочее
 
+
   // Пример: объявление константы времени компиляции в заголовочном файле
   inline constexpr auto kStringConstant = "Hello, stranger!";
 
   // Пример: объявление структуры с полями и методами
-  struct MyStructure {
-   public:
-    int size_{0};
-    int capacity_{0};
-    int* data_{nullptr};
+  template<class T>
+  class circular_buffer {
+   private:
+    size_t size_{0};
+    size_t capacity_{0};
+    T *data_{nullptr};
+
+    size_t tail_{0};
+    size_t head_{0};
+
+    void incrementHead();
+
+    void incrementTail();
+
+    void decrementHead();
+
+    void decrementTail();
 
     // Tip 2: На начальном этапе разработки структуры данных можете определения методов задавать в
     // заголовочном файле, как только работа будет завершена, можно будет оставить здесь только объявления.
+   public:
+    explicit circular_buffer(size_t max_size_) :
+            data_(new T[max_size_]), capacity_(max_size_) {}
 
-    int size() const {
-      return size_;
-    }
+    void EnqueueBack(T &elem);
+
+    void EnqueueFront(T &elem);
+
+    T DequeueBack();
+
+    T DequeueFront();
+
+    std::optional<T> getFront() const;
+
+    std::optional<T> getBack() const;
+
+    bool isFull() const;
+
+    bool isEmpty() const;
+
+    size_t size() const;
+
+    size_t capacity() const;
+
+    void Clear();
+
+    ~circular_buffer();
   };
 
 }  // namespace itis
--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
-#include "data_structure.hpp"
+#include "circular_buffer.hpp"
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-  for (int index = 0; index < argc; index++) {
-    cout << index << ": " << argv[index] << endl;
-  }
+int main() {
+  itis::circular_buffer<int> buf = itis::circular_buffer<int>(8);
+  int num = 1;
+  buf.EnqueueBack(num);
+  cout << buf.getBack().value() << ' ' << buf.getFront().value() << endl;
   return 0;
 }
